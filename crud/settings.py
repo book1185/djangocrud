0525b6424c1a9b3c65ede0f6cfc06268f16405ce
cha.aussarassakorn-a <charn.aussarassakorn@rakuten.com>
2022-11-09 10:42:49 +0900

    fixed

--- crud/settings.py
+++ crud/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://django-crudapi-practice.herokuapp.com/"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
